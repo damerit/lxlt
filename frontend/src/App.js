9adac448dbc3adf907ec118a3f79dd387b48a628
david <jonah@merrittintel.com>
2022-03-10 17:19:46 -0600

    estore-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">LxLite</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
